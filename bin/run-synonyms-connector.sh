5610a8fa9aa4b408149f3082c29102a7096de78b
Chad Johnson <chad@seearejay.com>
2019-02-04 12:15:40 -0700

    Updated packaging.

---
+++ bin/run-synonyms-connector.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+java \
+    -cp ./lib/sadasystems-gcs-synonyms-connector-1.0-withlib.jar \
+    com.sadasystems.gcs.connector.synonyms.SynonymsConnector \
+    -Dconfig=./properties/synonyms-connector.properties \
+    -Djava.util.logging.config.file=./properties/logging.properties
+
